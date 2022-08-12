e008f168684e8dad40999ca060500df4499822d8
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2022-08-12 20:29:12 +0530

    ready for server

--- gdsc/gdsc/settings.py
+++ gdsc/gdsc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z^6^!!3lp__)&f@wyya-8*5pb8u$9y)ny9^2dx_idvvwx&_#bs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
